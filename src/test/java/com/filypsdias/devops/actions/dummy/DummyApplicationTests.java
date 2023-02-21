cbf22ab7b3446c962fe4484560fd25e802581825
filypsdias <filipedias1708@gmail.com>
2023-02-20 23:37:02 -0300

    :recycle: Adding dummy spring project

---
+++ src/test/java/com/filypsdias/devops/actions/dummy/DummyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filypsdias.devops.actions.dummy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DummyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
